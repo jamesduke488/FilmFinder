3a0055661abe19f6000f82f62ede7adc75d78063
jamesduke488 <james@revolutionaries.ie>
2025-09-30 16:20:48 +0100

    Another attempt to fix deploy

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,10 +1,6 @@
-output "project_name" {
-  value = var.project_name
-}
+output "project_name" { value = var.project_name }
 
-output "artifact_bucket" {
-  value = aws_s3_bucket.artifacts.bucket
-}
+output "artifact_bucket" { value = aws_s3_bucket.artifacts.bucket }
 
 output "base_url" {
   value = aws_apigatewayv2_stage.prod.invoke_url
