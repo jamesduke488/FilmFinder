c07a44290e53f68173cd36dd736f9a2f3c873a70
jamesduke488 <james@revolutionaries.ie>
2025-09-30 16:28:11 +0100

    Made output pure expression

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
 output "project_name" { value = var.project_name }
 
-output "artifact_bucket" { value = aws_s3_bucket.artifacts.bucket }
+output "artifact_bucket" { value = "${var.project_name}-artifacts-${data.aws_caller_identity.me.account_id}" }
 
 output "base_url" {
   value = aws_apigatewayv2_stage.prod.invoke_url
